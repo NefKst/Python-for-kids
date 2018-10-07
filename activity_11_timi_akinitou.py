4200246f6c022422a46e3b29dca1df47843dd97c
Nefeli Kostakopoulou <39064484+nefkst@users.noreply.github.com>
2018-10-07 15:16:16 +0300

    Create activity_11_timi_akinitou.py

---
+++ activity_11_timi_akinitou.py
@@ -0,0 +1,22 @@
+ektasi = float(input("Πόσα τ.μ. είναι το ακίνητο;"))
+perioxi = str(input("Σε ποια περιοχή βρίσκεται το ακίνητο;"))
+if perioxi == "ΚΕΝΤΡΟ" or perioxi =="Κέντρο" or perioxi =="κέντρο":
+    proForou = ektasi * 700
+    foros = (proForou * 24) / 100
+    metaForou = proForou + foros
+    print ("Η τιμή του ακινήτου με τον φόρο είναι €" + str(metaForou))
+
+elif perioxi == "ΑΓΙΑ ΣΟΦΙΑ" or perioxi =="Αγία Σοφία" or perioxi =="αγία σοφία":
+    proForou = ektasi * 600
+    foros = (proForou * 24) / 100
+    metaForou = proForou + foros
+    print ("Η τιμή του ακινήτου με τον φόρο είναι €" + str(metaForou))
+
+elif perioxi == "ΡΙΟ" or perioxi =="Ρίο" or perioxi =="ρίο":
+    proForou = ektasi * 800
+    foros = (proForou * 24) / 100
+    metaForou = proForou + foros
+    print ("Η τιμή του ακινήτου με τον φόρο είναι €" + str(metaForou))
+
+else:
+    print("Η περιοχή δε βρέθηκε! \n Δοκιμάστε: ΚΕΝΤΡΟ, ΑΓΙΑ ΣΟΦΙΑ, ΡΙΟ")
